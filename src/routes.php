d0bf9709131777d17fd48977a8c4e60ffc651f16
chroda <chroda@chroda.com.br>
2017-09-17 17:15:09 -0300

    cleanup

--- src/routes.php
+++ src/routes.php
@@ -2,9 +2,5 @@
 // Routes
 
 $app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-
-    // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
