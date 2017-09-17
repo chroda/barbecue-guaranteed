341a5ed6103bce4850b2416837ba6f0fe42cfb3c
chroda <chroda@chroda.com.br>
2017-09-17 18:24:54 -0300

    basic views

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,38 @@
 <?php
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
+$app->get('/', function ($request, $response, $args) {
   return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+$app->get('/dashboard', function ($request, $response, $args) {
+  return $this->renderer->render($response, 'dashboard.phtml', $args);
+});
+
+$app->get('/register-company', function ($request, $response, $args) {
+  return $this->renderer->render($response, 'register-company.phtml', $args);
+});
+
+$app->get('/new-request', function ($request, $response, $args) {
+  return $this->renderer->render($response, 'new-request.phtml', $args);
+});
+
+$app->get('/my-account', function ($request, $response, $args) {
+  return $this->renderer->render($response, 'my-account.phtml', $args);
+});
+
+$app->get('/logout', function ($request, $response, $args) {
+  return $this->renderer->render($response, 'logout.phtml', $args);
+});
+
+$app->get('/login', function ($request, $response, $args) {
+  return $this->renderer->render($response, 'login.phtml', $args);
+});
+
+$app->get('/new-account', function ($request, $response, $args) {
+  return $this->renderer->render($response, 'new-account.phtml', $args);
+});
+
+// $app->get('/[{name}]', function ($request, $response, $args) {
+//   return $this->renderer->render($response, 'index.phtml', $args);
+// });
