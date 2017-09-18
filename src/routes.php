7411b8c9d42c7b5b606aa1996626d4b01b8f1298
chroda <chroda@chroda.com.br>
2017-09-17 21:07:42 -0300

    more views

--- src/routes.php
+++ src/routes.php
@@ -6,6 +6,24 @@ $app->get('/', function ($request, $response, $args) {
   return $response->withStatus(200)->withHeader('Location', 'login');
 });
 
+
+$app->get('/login', function ($request, $response, $args) {
+  return $this->renderer->render($response, 'login.phtml', $args);
+});
+$app->post('/login', function ($request, $response, $args) {
+  echo 'post login';
+  // return $this->renderer->render($response, 'login.phtml', $args);
+});
+
+$app->get('/new-account', function ($request, $response, $args) {
+  return $this->renderer->render($response, 'new-account.phtml', $args);
+});
+$app->post('/new-account', function ($request, $response, $args) {
+  echo 'post new-account';
+  // return $this->renderer->render($response, 'login.phtml', $args);
+});
+
+
 $app->get('/dashboard', function ($request, $response, $args) {
   return $this->renderer->render($response, 'dashboard.phtml', $args);
 });
@@ -13,9 +31,17 @@ $app->get('/dashboard', function ($request, $response, $args) {
 $app->get('/register-company', function ($request, $response, $args) {
   return $this->renderer->render($response, 'register-company.phtml', $args);
 });
+$app->post('/register-company', function ($request, $response, $args) {
+  echo 'post register-company';
+  // return $this->renderer->render($response, 'login.phtml', $args);
+});
 
-$app->get('/new-request', function ($request, $response, $args) {
-  return $this->renderer->render($response, 'new-request.phtml', $args);
+$app->get('/new-order', function ($request, $response, $args) {
+  return $this->renderer->render($response, 'new-order.phtml', $args);
+});
+$app->post('/new-order', function ($request, $response, $args) {
+  echo 'post new-order';
+  // return $this->renderer->render($response, 'login.phtml', $args);
 });
 
 $app->get('/my-account', function ($request, $response, $args) {
@@ -23,16 +49,10 @@ $app->get('/my-account', function ($request, $response, $args) {
 });
 
 $app->get('/logout', function ($request, $response, $args) {
-  return $this->renderer->render($response, 'logout.phtml', $args);
+  // unlogged current user
+  return $response->withStatus(200)->withHeader('Location', 'login');
 });
 
-$app->get('/login', function ($request, $response, $args) {
-  return $this->renderer->render($response, 'login.phtml', $args);
-});
-
-$app->get('/new-account', function ($request, $response, $args) {
-  return $this->renderer->render($response, 'new-account.phtml', $args);
-});
 
 // $app->get('/[{name}]', function ($request, $response, $args) {
 //   return $this->renderer->render($response, 'index.phtml', $args);
